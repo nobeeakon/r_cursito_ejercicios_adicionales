5bb2c8c171b990b4da2466dda50bd6f129092292
Daniel T <dtorresgonzalez@pentalog.com>
2021-12-19 09:14:35 -0600

    respuestas y ejercicios 0

---
+++ ejercicios/intro/respuestas.R
@@ -0,0 +1,41 @@
+# Respuestas 
+
+### Operaciones 
+## 1
+
+2+2
+
+3-3
+
+3*3
+
+3/3
+
+3^2
+
+(3*3)-3
+
+( (3*3) / (3 - 2^(4-2))) + 4
+
+2^3
+
+(-2)^2
+
+
+### Variables
+## 1
+
+perro <- 5
+gato <- 10
+simio <- 2 * gato
+mamifero <- (simio - gato) / perro^2
+
+mamifero
+
+
+## 2
+
+x <- 44
+y <- x^2
+z <- (2 * y - 80)/y
+z
